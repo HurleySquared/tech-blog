f7b5cfbcc9741410d8f575b1c1ca44bb10ee836e
Eric Hurley <erichurley421@gmail.com>
2021-03-25 21:01:42 -0400

    Added to homeRoutes and homepage.handlebars

--- controllers/api/homeRoutes.js
+++
@@ -1,2 +0,0 @@
-// Write all home routes, get blog posts and comments
-//  on homepage if the user is logged in
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,9 @@
-module.exports = {
-  // TODO: add helper functionality when I get to it
+const withAuth = (req, res, next) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
 };
+
+module.exports = withAuth;
